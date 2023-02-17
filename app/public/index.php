e4b387fc1bcc44058df2649c8a982025cef7e0a9
Yvan Roes <690101@student.inholland.nl>
2023-02-17 14:40:58 +0100

    .

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,15 @@
-Hello
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require __DIR__ . '/../routers/patternRouter.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header("Access-Control-Allow-Headers: *");
+
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+$router = new PatternRouter();
+$router->route($uri);;
+
