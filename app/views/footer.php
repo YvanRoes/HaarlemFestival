1fafc0b412f9443f9bc4115a4993ac62b3968491
cryphon <yvanroes@gmail.com>
2023-02-21 20:13:40 +0100

    made a start on the footer

---
+++ app/views/footer.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<script src="https://cdn.tailwindcss.com"></script>
+
+<body class="bg[#F7F7FB] h-screen">
+  <footer class="w-screen h-[300px] bg-[#D0E9F7] flex items-center gap-[20px] justify-center">
+    <div class="w-maxcontent h-[175px] bg-[#F7F7FB8A] rounded-[15px] flex justify-center flex-row gap-16 leading-5 p-6 lg:pl-[75px] lg:pr-[75px] md:pl-[20px] sm:flex-row" id="Navigation">
+      <ul class="list-none flex flex-col">
+        <h2 class="font-extrabold mb-2">Culture</h2>
+        <li class="hover:underline cursor-pointer">History</li>
+        <li class="hover:underline cursor-pointer">Museums</li>
+        <li class="hover:underline cursor-pointer">Theatre</li>
+        <li class="hover:underline cursor-pointer">Monuments</li>
+      </ul>
+      <ul class="list-none flex flex-col">
+        <h2 class="font-extrabold mb-2">Food</h2>
+        <li class="hover:underline cursor-pointer">Breakfast</li>
+        <li class="hover:underline cursor-pointer">Lunch</li>
+        <li class="hover:underline cursor-pointer">Dinner</li>
+        <li class="hover:underline cursor-pointer">Restaurants</li>
+        <li class="hover:underline cursor-pointer">Bars</li>
+      </ul>
+      <ul class="list-none flex flex-col">
+        <h2 class="font-extrabold mb-2">Kids</h2>
+        <li class="hover:underline cursor-pointer">Museums</li>
+        <li class="hover:underline cursor-pointer">Activities</li>
+      </ul>
+    </div>
+    <div class="w-[20%] h-[150px] bg-[#F7F7FB8A] rounded-[15px]" id="Social media"></div>
+  </footer>
+</body>
+
+</html>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+  *{
+    font-family: 'Lato', sans-serif;
+  }
+</style>
 
-<body>
-  Hello world Homepage
-</body>
-
-</html>
+<?php
+include __DIR__ . '/../footer.php';
+?>
