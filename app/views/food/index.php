e4a9bb8ba4bfc9cfc8839cebb9125a66cb07e26d
Jason Xie <87727651+JasonXie62@users.noreply.github.com>
2023-02-22 19:06:49 +0100

    start with yummie subpage

---
+++ app/views/food/index.php
@@ -0,0 +1,48 @@
+<html lang="en">
+<script src="https://cdn.tailwindcss.com"></script>
+
+<body class="">
+    <div id="introSection" class="grid grid-cols-2 grid justify-items-end flex items-center pt-[50px]">
+        <div id="introduction" class="">
+            <h1 class="text-[42px] font-bold"><span class="text-[#42BFDD]">Yummie!</span> Food Event</h1>
+            <p class="text-2xl">27 July - 31 July</p>
+            <p class="text-base w-[700px] pt-[25px]">Welcome to the Haarlem Food Festival! Come and join us for an
+                amazing
+                culinary experience.
+                This event is a celebration of the culinary delights that the Netherlands has to offer,
+                showcasing different cuisines and the talented chefs who prepare it. <br><br>
+                We look forward to seeing you there!</p>
+            <p class="text-sm text-[#656262] w-[300px] pt-[5px]">** €10,- deposit pp. and mandatory reservation. Deposit
+                will be
+                deducted upon payment.**</p>
+        </div>
+        <div id="introImage" class="pr-[100px]">
+            <img class="w-[400px] h-[400px]" src="/img/circleFoodImage.png" alt="circleFoodImage">
+        </div>
+    </div>
+    <div id="restaurants" class="grid grid-cols-2 pl-[235px]">
+        <div id="ratatouilleSection">
+            <div class="flex">
+                <h1 class="text-3xl font-bold pl-[15px]">Ratatouille</h1>
+                <img class="w-[24px] h-[26px] mt-[5px] ml-[5px]" src="/img/michelinIcon.png" alt="michelinIcon">
+            </div>
+            <div class="flex justify-items-start pl-[15px]">
+                <img class="w-[115px] h-[25px]" src="/img/ratatouilleRating.png" alt="rating">
+                <p class="text-[20px] font-medium pl-[10px]">4.7/5</p>
+                <p class="text-[20px] text-[#FC5B84] font-medium pl-[10px]"> French, Seafood, European</p>
+            </div>
+            <div class="grid-cols-2 grid justify-items-end">
+                <img class="w-[600px] h-[500px]" src="/img/ratatouilleImage1.png" alt="ratatouilleImage1">
+                <div>
+                    <img class="w-[500px] h-[250px]" src="/img/ratatouilleImage2.png" alt="ratatouilleImage1">
+                    <img class="w-[500px] h-[255px]" src="/img/ratatouilleImage3.png" alt="ratatouilleImage1">
+                </div>
+            </div>
+        </div>
+        <div id="reservationPopup">
+
+        </div>
+    </div>
+</body>
+
+</html>
