f423067498628a6afcd3b7dae32f94fc1ac2ca25
cryphon <yvanroes@gmail.com>
2023-03-07 20:30:56 +0100

    changed some colour values in header

--- app/public/js/userPanel/script.js
+++ app/public/js/userPanel/script.js
@@ -1,44 +1,50 @@
 async function loadData() {
-    parentElement = document.getElementById('contentItemsWrapper');
-    parentElement.innerHTML = '';
-  
-    let objects;
-    const response = await fetch(
-      'http://localhost/api/users'
+  parentElement = document.getElementById('contentItemsWrapper');
+  parentElement.innerHTML = '';
+
+  let objects;
+  const response = await fetch('http://localhost/api/users');
+  objects = await response.json();
+  for (let i = 0; i < objects.length; i++) {
+    createUserContainer(
+      parentElement,
+      objects[i].id,
+      objects[i].username,
+      objects[i].email,
+      objects[i].password
     );
-    objects = await response.json();
-    for (let i = 0; i < objects.length; i++) {
-      createUserContainer(
-        parentElement,
-        objects[i].id,
-        objects[i].username,
-        objects[i].email,
-        objects[i].password
-      );
-    }
   }
+}
 
-function createUserContainer(element, id, username, email){
+function createUserContainer(element, id, username, email) {
+  container = document.createElement('span');
+  container.classList.add(
+    'bg-white',
+    'p-2',
+    'rounded-md',
+    'grid',
+    'lg:grid-cols-3',
+    'md:grid-cols-1',
+    'text-left'
+  );
+  idSpan = document.createElement('span');
+  idSpan.innerHTML = id;
+  idSpan.classList.add('col-span-1');
+  idSpan.contentEditable = true;
 
-    container = document.createElement('span');
-    container.classList.add('bg-white', 'p-2','rounded-md','grid','lg:grid-cols-3', 'md:grid-cols-1', 'text-left');
-    idSpan = document.createElement('span');
-    idSpan.innerHTML = id;
-    idSpan.classList.add('col-span-1')
+  unameSpan = document.createElement('span');
+  unameSpan.innerHTML = username;
+  unameSpan.classList.add('col-span-1');
 
-    unameSpan = document.createElement('span');
-    unameSpan.innerHTML = username;
-    unameSpan.classList.add('col-span-1');
+  mailSpan = document.createElement('span');
+  mailSpan.innerHTML = email;
+  mailSpan.classList.add('col-span-1');
 
-    mailSpan = document.createElement('span');
-    mailSpan.innerHTML = email;
-    mailSpan.classList.add('col-span-1');
+  container.appendChild(idSpan);
+  container.appendChild(unameSpan);
+  container.appendChild(mailSpan);
 
-    container.appendChild(idSpan);
-    container.appendChild(unameSpan);
-    container.appendChild(mailSpan);
-
-    element.appendChild(container);
+  element.appendChild(container);
 }
 
 loadData();
