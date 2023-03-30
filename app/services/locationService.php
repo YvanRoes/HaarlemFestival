cfce22524b7222be6ee0d181c9d69b7873005249
cryphon <yvanroes@gmail.com>
2023-03-30 03:19:07 +0200

    added venue crud

--- app/services/locationService.php
+++ app/services/locationService.php
@@ -8,4 +8,19 @@ class LocationService
     $repo = new LocationRepository();
     return $repo->get_AllLocations();
   }
+
+  public function insert_location(Location $location){
+    $repo = new LocationRepository();
+    return $repo->insert_location($location);
+  }
+
+  public function delete_locationById($id){
+    $repo = new LocationRepository();
+    return $repo->delete_locationById($id);
+  }
+
+  public function edit_locationById($id, $name, $address, $cap){
+    $repo = new LocationRepository();
+    return $repo->edit_locationById($id, $name, $address, $cap);
+  }
 }
