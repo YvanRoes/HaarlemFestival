b9c44fac09cfca218e2df8bb983a0933de1d83b6
Jason Xie <87727651+JasonXie62@users.noreply.github.com>
2023-04-06 21:53:38 +0200

    manage user done

--- app/controllers/userPanelController.php
+++ app/controllers/userPanelController.php
@@ -1,12 +1,26 @@
 <?php
     require_once __DIR__ . '/controller.php';
+    require_once __DIR__ . '/../models/user.php';
+    require_once __DIR__ . '/../services/userService.php';
+    require_once __DIR__ . '/../services/mailService.php';  
+
     class UserPanelController extends Controller{
+        private $service;
+
+        public function __construct(){
+            $this->service = new UserService();
+        }
+
         public function index(){
+            error_reporting(E_ERROR | E_PARSE);
             if(!isset($_SESSION['USER_ID'])){
                 header('Location: /home');
                 return;
             }
-            //$this->displayView($this);
+
+            $userID = $_SESSION['USER_ID'];
+            $user = $this->service->get_UserById($userID);
+
             $userRole = $_SESSION['USER_ROLE'];
             switch($userRole){
                 case 0:
@@ -22,6 +36,24 @@
                     echo "unknown";
                     break;
             }
+
+            if($_SERVER['REQUEST_METHOD'] == "POST"){ 
+                $this->updateUser();
+
+                $_SESSION['editMsg'] = 'Account updated successfully';
+                return;
+            }
+
+        }
+
+        public function updateUser(){
+            $userID = $_SESSION['USER_ID'];
+            $username = htmlspecialchars($_POST['username']);
+            $email = htmlspecialchars($_POST['email']);
+
+            //send confirmation email
+            sendMail($_SESSION['USER_MAIL'], 'Account updated', 'Your account has been updated. <br> If this is not you please change your password. <br> <a href="http://localhost/home">Change Password</a>', 'Your account has been updated');
+            $this->service->update_UsernameAndEmail($userID, $username, $email);  
         }
     }
 
--- app/views/userPanel/customerPanel.php
+++ app/views/userPanel/customerPanel.php
@@ -1,3 +1,12 @@
+<?php
+
+$msg = '';
+if (isset($_SESSION['editMsg'])) {
+    $msg = $_SESSION['editMsg'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,22 +25,32 @@
     include __DIR__ . '/../header.php';
     generateHeader('home', 'dark');
     ?>
-    <div class="w-screen h-fit grid grid-cols-6 grid-rows-6 mt-[100px]">
+    <div class="w-screen h-fit  mt-[100px] mb-[100px]">
         <div class="col-span-6 row-span-1 border-y-[1px] border-[#656262]" id="header">
             <div class="flex items-center justify-center h-[100%]" id="nav">
-                <ul class="decoration-none flex items-center justify-center flex-row gap-[20px] text-[#656262]">
-                    <li class="transition ease-in-out hover:translate-y-[-5px] hover:text-[#121212]"><a href="">User information</a></li>
+                <ul class="decoration-none flex items-center justify-center flex-row gap-[20px] text-[#656262] mt-[20px] mb-[20px]">
+                    <li class="transition ease-in-out hover:translate-y-[-5px] hover:text-[#121212] ">User information</a></li>
                     <li class="transition ease-in-out hover:translate-y-[-5px] hover:text-[#121212]"><a href="">Personal Program</a></li>
                 </ul>
             </div>
         </div>
-        <div class="flex flex-col gap-4 w-screen justify-center" id="contentItemsWrapper">
-                <button name="changePassword" type="submit" class="text-[#29334E] text-3xl underline mt-4"><a href='/resetPassword'>Change Password</button>
-            </div>
     </div>
-    </div>
-    </div>
-    </div>
+
+    <form method="POST" action="" class="flex flex-col items-center">
+            <h1 class="flex items-center justify-center text-3xl">Personal Information</h1>
+            <input name="username" value="<?echo $user->get_username()?>" rows="1"
+                class="block h-[40px] w-[300px] mt-[50px] m-auto my-4 py-2 px-2 text-sm text-gray-800 bg-[#F7F7FB] border-0 rounded-sm focus:ring-0 resize-none outline outline-black outline-[1px]"
+                placeholder="username" required></input>
+            <input name="email" value="<?echo $user->get_email()?>" rows="1"
+                class="block h-[40px] w-[300px] m-auto my-4 py-2 px-2 text-sm text-gray-800 bg-[#F7F7FB] border-0 rounded-sm focus:ring-0 resize-none outline outline-black outline-[1px]"
+                placeholder="email" required></input>
+            <button name="changePassword" type="submit" class=" hover:text-[#d3d3d3] text-[#29334E] text-xl underline"><a href='/resetPassword'>Change Password</button>    
+            <h2 class="flex items-center justify-center text-[#FC5B84] mt-[15px] ">
+                    <?php echo $msg; ?>
+            </h2>  
+            <button type="submit" class="w-[300px] h-[40px] mt-[20px] bg-[#42BFDD] text-white text-[20px] font-bold rounded-[10px] cursor-pointer">Edit Account</button>
+    </form>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 
 </body>
